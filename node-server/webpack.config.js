b5d9bc07c4d44ae9a43ec0f10a78504971ac82d9
uknmr <kanamori@mediba.jp>
2017-05-21 21:29:03 +0900

    :wrench: React SSR ビルド環境を作成。
    - Webpack と babel-loader
    - 開発時は --watch と nodemon

---
+++ node-server/webpack.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack')
+const { resolve } = require('path')
+const nodeExternals = require('webpack-node-externals')
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  entry: './index',
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'server.js',
+    publicPath: '/',
+    // エントリポイントの戻りを module.exports の形で提供する
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }
+      }
+    ]
+  },
+  target: 'node',
+  // 全モジュール bundle せずに環境から呼び出す
+  externals: [nodeExternals()],
+  plugins: isProduction ? [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    })
+  ] : [],
+  node: {
+    // Node.js サーバー以外で動かす予定はない
+    console: false,
+    global: false,
+    process: false,
+    __filename: false,
+    __dirname: false,
+    Buffer: false,
+    setImmediate: false,
+  }
+}
