013260f84807cbdee5ce535ee5f8b3cbf5b04de6
uknmr <kanamori@mediba.jp>
2017-07-05 12:43:31 +0900

    :sparkles: Webhook の内容をメールで送信する

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -1,8 +1,46 @@
 const functions = require('firebase-functions');
+const nodemailer = require('nodemailer')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const gmailEmail = functions.config().gmail.email
+const gmailPassword = functions.config().gmail.password
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword,
+  }
+})
+
+exports.githubWebhook = functions.https.onRequest((req, res) => {
+  const { action } = req.body
+
+  if (action !== 'opened') {
+    console.log(`opened じゃないので除外したよ👍: ${action}`)
+    res.end()
+    return
+  }
+
+  const targetEmail = functions.config().target.email
+  const { issue, repository } = req.body
+
+  sendTalknote({
+    from: gmailEmail,
+    to: targetEmail,
+    text: `
+      eureka に新しい投稿がありました🎉 by ${issue.user.login}\
+      \n
+      # ${issue.title}
+      ${issue.html_url}
+    `,
+  })
+
+  res.end()
+})
+
+const sendTalknote = data => {
+  transporter.sendMail(data)
+    .then(console.log(`🚀${data.to} にメールした`))
+    .catch(error => {
+      console.error(error)
+    })
+}
